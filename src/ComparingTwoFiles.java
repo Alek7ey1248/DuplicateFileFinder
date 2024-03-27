06a738183a0c61c66ed446a20f482a5cf4c5a94f
alek7ey <alek7ey1248@gmail.com>
2024-03-27 17:34:06 +0200

    При помощи потоков оптимизировал метод findDuplicates класса FileDuplicateFinder. Также в методе calculateContentHash класса ComparingTwoFiles сделал проверку файла на валидность , в случаее невалидности метод возвращает -10 и потом группу файлов в HashMap метода findDuplicates по ключу -10 удаляет

--- src/ComparingTwoFiles.java
+++ src/ComparingTwoFiles.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.nio.file.Files;
 import java.security.MessageDigest;
 import java.io.FileInputStream;
 import java.security.NoSuchAlgorithmException;
@@ -19,6 +18,11 @@ public class ComparingTwoFiles {
     // числовое представление содержимого файла, которое используется для
     // быстрого сравнения файлов на их эквивалентность.
     public int calculateContentHash(File file) {
+
+        if (!isValidFile(file)) {
+            return -10;
+        }
+
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             FileInputStream fis = new FileInputStream(file);
@@ -42,66 +46,23 @@ public class ComparingTwoFiles {
 
             return hash;
         } catch (IOException | UncheckedIOException e) {
-            System.out.println("Помилка читання файлу: " + e.getMessage());
+            System.out.println(" Помилка читання файлу в методе calculateContentHash: " + e.getMessage());
             return 0;
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
     }
 
-
-
-
-    public boolean areFilesEqual(File file1, File file2) {
-        if (!isValidFile(file1) || !isValidFile(file2)) {
-            return false;
-        }
-
-        byte[] content1;
-        byte[] content2;
-        try {
-            content1 = Files.readAllBytes(file1.toPath());
-            content2 = Files.readAllBytes(file2.toPath());
-        } catch (IOException | UncheckedIOException e) {
-            System.out.println("Помилка читання файлу: " + e.getMessage());
-            return false;
-        }
-
-        if (content1.length != content2.length) {
-            return false;
-        }
-
-        return compareFilesContent(content1, content2);
-    }
-
-
-    private boolean compareFilesContent(byte[] content1, byte[] content2) {
-        for (int i = 0; i < content1.length; i++) {
-            if (content1[i] != content2[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
     private boolean isValidFile(File file) {
         if (!file.isFile()) {
-            System.out.println("File " + file.getAbsolutePath() + " не є файлом.");
+            System.out.println("Метод isValidFile.    File " + file.getAbsolutePath() + " не є файлом.");
             return false;
         }
         if (!file.canRead()) {
-            System.out.println("File " + file.getAbsolutePath() + " пошкоджений.");
+            System.out.println("Метод isValidFile.     File " + file.getAbsolutePath() + " пошкоджений.");
             return false;
         }
         return true;
     }
 
-//    public static void main(String[] args) {
-//        File file1 = new File("/home/alek7ey/Рабочий стол/TestsDuplicateFileFinder/test11/test12/photo_2021-12-09_16-12-54 (копия).jpg");
-//        File file2 = new File("/home/alek7ey/Рабочий стол/TestsDuplicateFileFinder/test11/photo_2021-12-09_16-12-54.jpg");
-//        ComparingTwoFiles comparator = new ComparingTwoFiles();
-//        boolean result = comparator.areFilesEqual(file1, file2);
-//        //System.out.println("UM_THREADS - " + FileUtils.NUM_THREADS);
-//        System.out.println("Файлы равны: " + result);
-//    }
 }
--- src/FileDuplicateFinder.java
+++ src/FileDuplicateFinder.java
@@ -1,182 +1,92 @@
 import java.io.File;
 import java.util.*;
-
-// Клас для пошуку дублікатів файлів:
-//- Методи для пошуку дублікатів файлів у зазначеній директорії та її піддиректоріях.
-//- Методи для порівняння файлів на основі вмісту.
-//- Методи для угруповання та виведення результатів.
-
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
 
-// Клас для пошуку файлів у зазначеній директорії та її піддиректоріях
-// та пошуку груп дублікатів файлів.
 public class FileDuplicateFinder {
-    private ComparingTwoFiles fileUtils;
+
+    private ComparingTwoFiles comparingTwoFiles;
 
     public FileDuplicateFinder() {
-        this.fileUtils = new ComparingTwoFiles();
+        this.comparingTwoFiles = new ComparingTwoFiles();
     }
 
-    // Основной метод поиска групп дубликатов
-    // полученый список файлов упорядочиваем для оптимизации поиска
-    // и методом findDuplicates ищем группы дубликатов
-    public List<Set<File>> groupingAndFindDuplicates(Set<File> setAllFiles) {
-
-        // конвертировать в List
-        List<File> listAllFiles = new ArrayList<>(setAllFiles);
-
-        // упорядочить по размеру
-        sortBySize(listAllFiles);
-//        System.out.println("--после упорядочения -- ");
-//        for (File f : listAllFiles) {
-//            System.out.println(f.getName() + " - " + f.length());
+//    public List<List<File>> findDuplicates(List<File> fileList) {
+//        Map<Integer, List<File>> contentHashGroups = new HashMap<>();
+//        List<List<File>> duplicateGroups = new ArrayList<>();
+//
+//        for (File file : fileList) {
+//            int contentHash = comparingTwoFiles.calculateContentHash(file);
+//            if (contentHash == -10) continue;  // в методе calculateContentHash -10 если невалидный файл
+//            if (contentHashGroups.containsKey(contentHash)) {
+//                contentHashGroups.get(contentHash).add(file);
+//            } else {
+//                List<File> newGroup = new ArrayList<>();
+//                newGroup.add(file);
+//                contentHashGroups.put(contentHash, newGroup);
+//            }
 //        }
+//
+//        return filterAndSortDuplicateGroups(contentHashGroups);
+//    }
 
-        // распределение по группам файлов одного размера и удаление групп с одним файлом
-        List<Set<File>> groupsBySize = groupsBySize(listAllFiles);
-        int totalFilesCount = groupsBySize.stream()
-                .mapToInt(Set::size)
-                .sum();
-        System.out.println("всего сгруппированных файлов без уникальных по размеру: " + totalFilesCount);
 
-        // в каждой группе файлов с одинаковым размером ищем группы дубликатов и добавляем в resGroup
-//        List<Set<File>> resGroup = new ArrayList<>();
-//        for (Set<File> set : groupsBySize) {
-//            List<File> lf = new ArrayList<>(set);
-//            System.out.println(" проверяются файлы размера - " + + lf.get(0).length() + " - " + lf.get(0).getName());
-//            List<Set<File>> lsf = findDuplicates(lf);
-//            resGroup.addAll(lsf);
-//        }
-        //-------------------------------------------------- быстрее чем верхняя реализ в 2, 3 раза
-        List<Set<File>> resGroup = groupsBySize.parallelStream()
-                .map(set -> {
-                    List<File> lf = new ArrayList<>(set);
-                    System.out.println(" проверяются файлы размера - " + + lf.get(0).length() + " - " + lf.get(0).getName());
-                    return findDuplicates(lf);
-                })
-                .flatMap(List::stream)
-                .collect(Collectors.toList());
-        //------------------------------------------------
-        return resGroup;
+    public List<List<File>> findDuplicates(List<File> fileList) {
+        Map<Integer, List<File>> contentHashGroups = fileList.parallelStream()
+                //.filter(file -> comparingTwoFiles.calculateContentHash(file) != -10)
+                .collect(Collectors.groupingByConcurrent(comparingTwoFiles::calculateContentHash));
+
+        contentHashGroups.remove(-10); // в методе calculateContentHash -10 если невалидный файл
+
+        return filterAndSortDuplicateGroups(contentHashGroups);
     }
 
 
-    // Сортировка файлов по размеру от большего к меньшему.
-    // Вспомогательный метод.
-    private List<File> sortBySize(List<File> list) {
-        Collections.sort(list, new Comparator<File>() {
-            public int compare(File file1, File file2) {
-                return Long.compare(file2.length(), file1.length());
-            }
-        });
-        return list;
-    }
+    // из HashMap все группы (> 1 файла) в List<List<File>> и сортируем
+    private List<List<File>> filterAndSortDuplicateGroups(Map<Integer, List<File>> contentHashGroups) {
+        List<List<File>> duplicateGroups = new ArrayList<>();
 
-    // Распределение по группам файлов одного размера и удаление групп с одним элементом.
-    // Вспомогательный метод.
-    private List<Set<File>> groupsBySize(List<File> sortListAllFiles) {
-        List<Set<File>> groups = new ArrayList<>();
-        Set<File> setF = new HashSet<>();
-        File previousFile = new File("");
-        setF.add(previousFile);
-        for (int i = 0; i < sortListAllFiles.size(); i++) {
-            File currFile = sortListAllFiles.get(i);
-            if (currFile.length() == previousFile.length()) {
-                setF.add(currFile);
-            } else {
-                setF = new HashSet<>();
-                groups.add(setF);
-                setF.add(currFile);
-            }
-            previousFile = currFile;
-        }
-
-        // убираем группы где по одному файлу
-        List<Set<File>> optimGroups = new ArrayList<>();
-        for (Set<File> list : groups) {
-            if (list.size() > 1) optimGroups.add(list);
-        }
-        return optimGroups;
-    }
-
-
-    // Метод - поиск групп дубликатов файлов из списка файлов.
-    // Вспомогательный. Но может быть как основной метод( очень медленный и неоптимальный).
-    // ----------------------------
-    public List<Set<File>> findDuplicates(List<File> fileList) {
-        Map<File, Boolean> checkedFiles = new HashMap<>();
-        Set<Set<File>> duplicateGroups = new HashSet<>();
-
-        for (File file : fileList) {
-            if (checkedFiles.containsKey(file)) {
-                continue; // Пропускаем файл, если уже был добавлен в другую группу
-            }
-
-            Set<File> group = new HashSet<>();
-            group.add(file);
-            for (File otherFile : fileList) {
-                if (file.equals(otherFile) || checkedFiles.containsKey(otherFile)) {
-                    continue;
-                }
-                if (fileUtils.areFilesEqual(file, otherFile)) {
-                    group.add(otherFile);
-                    checkedFiles.put(otherFile, true);
-                }
-            }
-
+        for (List<File> group : contentHashGroups.values()) {
             if (group.size() > 1) {
                 duplicateGroups.add(group);
             }
         }
 
-        return new ArrayList<>(duplicateGroups);
-    }
-    // ----------------------------
-//    public List<Set<File>> findDuplicates(List<File> fileList) {
-//
-//        List<Set<File>> duplicateGroups = new ArrayList<>();
-//        List<File> checkedDuplicateGroups = new ArrayList<>();
-//
-//        for (int i = 0; i < fileList.size(); i++) {
-//            if (checkedDuplicateGroups.contains(fileList.get(i))) {
-//                continue; // Пропускаем файл, если уже был добавлен в другую группу
-//            }
-//            Set<File> group = new HashSet<>();
-//            if (!duplicateGroups.contains(group)) {
-//                group.add(fileList.get(i));
-//                for (int j = i + 1; j < fileList.size(); j++) {
-//                    if (fileUtils.areFilesEqual(fileList.get(i), fileList.get(j))) {
-//                        group.add(fileList.get(j));
-//                        checkedDuplicateGroups.add(fileList.get(j));
-//                    }
-//                }
-//                if (group.size() > 1) {
-//                    duplicateGroups.add(group);
-//                }
-//            }
-//        }
-//        return duplicateGroups;
-//
-//    }
+        Collections.sort(duplicateGroups, new FileListSizeComparator());
 
-    // поиск всех файлов в указаной директории
-    public Set<File> listFilesInTheDirectory(File directory) {
-        Set<File> fileList = new HashSet<>();
-        File[] files = directory.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    fileList.addAll(listFilesInTheDirectory(file));
-                } else {
-                    fileList.add(file);
-                }
-            }
-        }
-        return fileList;
+        return duplicateGroups;
+    }
+
+
+    public static void main(String[] args) {
+
+        long startTime = System.nanoTime();
+
+        String[] paths = new String[5];
+        paths[0] = "/home/alek7ey/Рабочий стол/TestsDuplicateFileFinder/test11";
+        paths[1] = "/home/alek7ey/Рабочий стол/TestsDuplicateFileFinder/test11/test12";
+        paths[2] = "/home/alek7ey/Рабочий стол/TestsDuplicateFileFinder/test11/test12/test13";
+        paths[3] = "/home/alek7ey/Рабочий стол/TestsDuplicateFileFinder/test21";
+        //paths[4] = "/home/alek7ey/snap/telegram-desktop";
+        paths[4] = "/home";
+
+        // проверим валидность аргументов
+        ArgumentsProcessor processor = new ArgumentsProcessor();
+        List<String> validPaths = processor.processArguments(paths);
+
+        FileDuplicateFinder fdf = new FileDuplicateFinder();
+        AllFilesDirectory allFD = new AllFilesDirectory();
+
+        allFD.findAllFilesInDirectories(validPaths);
+        List<File> fileList = allFD.getFileList();
+        List<List<File>> duplicates = fdf.findDuplicates(fileList);
+
+        PrintFileGroup printFileGroup = new PrintFileGroup();
+        printFileGroup.printDuplicateGroups(duplicates);
+
+
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime) / 1_000_000; // Перевод наносекунд в миллисекунды
+        System.out.println("Время выполнения программы: " + duration + " мс");
     }
 
 }
-
