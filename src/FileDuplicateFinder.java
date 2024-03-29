f6012c43fea53f3573c63ebaf58dd562128135aa
alek7ey <alek7ey1248@gmail.com>
2024-03-29 19:22:34 +0200

    метод findDuplicates класса FileDuplicateFinder поменял для того чтобы избежать одинаковых ключей изза много поточности при помощи synchronized. Старый метод внизу в коментариях и ниже вариант избегания повторных ключей с помощью merge

--- src/FileDuplicateFinder.java
+++ src/FileDuplicateFinder.java
@@ -1,10 +1,12 @@
 import java.io.File;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 
 public class FileDuplicateFinder {
 
-    private final Hashing hashing;
+    private Hashing hashing;
 
     public FileDuplicateFinder() {
         this.hashing = new Hashing();
@@ -16,15 +18,19 @@ public class FileDuplicateFinder {
     // Потом методом filterAndSortDuplicateGroups перегоняет
     // в List<List<File>> и сортирует.
     public List<List<File>> findDuplicates(List<File> fileList) {
+        Map<Integer, List<File>> contentHashGroups = Collections.synchronizedMap(new HashMap<>());
 
-        Map<Integer, List<File>> contentHashGroups = fileList.parallelStream()
-                .collect(Collectors.groupingByConcurrent(hashing::calculateHashWithSize));
-
-        contentHashGroups.remove(-1); // в методе calculateContentHash -1 если невалидный файл
+        fileList.parallelStream().forEach(file -> {
+            int hash = hashing.calculateHashWithSize(file);
+            if (hash != -1) {
+                synchronized (contentHashGroups) {
+                    contentHashGroups.computeIfAbsent(hash, k -> new ArrayList<>()).add(file);
+                }
+            }
+        });
 
         return filterAndSortDuplicateGroups(contentHashGroups);
     }
-
 
     // из HashMap все группы (> 1 файла) в List<List<File>> и сортирует
     private List<List<File>> filterAndSortDuplicateGroups(Map<Integer, List<File>> contentHashGroups) {
@@ -33,7 +39,11 @@ public class FileDuplicateFinder {
 
         for (List<File> group : contentHashGroups.values()) {
             if (group.size() > 1) {
+                //System.out.println("Группа размер " + group.get(0).length() + "   -----------------------");
                 duplicateGroups.add(group);
+//                for (File f : group) {
+//                    System.out.println(f.getName() + " размер - " + f.length());
+//                }
             }
         }
 
@@ -42,5 +52,36 @@ public class FileDuplicateFinder {
         return duplicateGroups;
     }
 
+//   первый вариант метода - !!! возможны одинаковые ключи изза многопоточности
+//        public List<List<File>> findDuplicates(List<File> fileList) {
+//
+//        Map<Integer, List<File>> contentHashGroups = fileList.parallelStream()
+//                .collect(Collectors.groupingByConcurrent(hashing::calculateHashWithSize));
+//
+//        contentHashGroups.remove(-1); // в методе calculateContentHash -1 если невалидный файл
+//
+//        return filterAndSortDuplicateGroups(contentHashGroups);
+//    }
+
+
+
+
+  //второй вариант метода чтобы небыло одинаковых ключей ( используя merge)
+//    public List<List<File>> findDuplicates(List<File> fileList) {
+//        ConcurrentHashMap<Integer, List<File>> contentHashGroups = new ConcurrentHashMap<>();
+//
+//        fileList.parallelStream().forEach(file -> {
+//            int hash = hashing.calculateHashWithSize(file);
+//            if (hash != -1) {
+//                contentHashGroups.merge(hash, new CopyOnWriteArrayList<>(Arrays.asList(file)), (list1, list2) -> {
+//                    list1.addAll(list2);
+//                    return list1;
+//                });
+//            }
+//        });
+//
+//        return filterAndSortDuplicateGroups(contentHashGroups);
+//    }
+//
 }
 
